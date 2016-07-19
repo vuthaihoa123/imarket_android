666d269e8d68ce7fa8161196270047e7b6f9dfcd
framgia <framgia@framgia0042-lt.framgia.com>
2016-07-19 08:58:48 +0700

    initialize project

---
+++ app/src/main/java/com/example/framgia/imarketandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.framgia.imarketandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
