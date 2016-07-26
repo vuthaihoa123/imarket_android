6e1ff5ed8e62c8d75f9c98a9fba02ed28af87d43
nguyenvantoan <toannguyen201194@gmail.com>
2016-07-26 09:58:24 +0700

    task012_profile_update

--- app/src/main/java/com/example/framgia/imarketandroid/util/Constants.java
+++ app/src/main/java/com/example/framgia/imarketandroid/util/Constants.java
@@ -16,4 +16,5 @@ public class Constants {
     public static final String PACKAGE="package";
     public static final String KEYHASH="KeyHash:";
     public static final String SHA="SHA";
+    public static final String PROFILE="Profile";
 }
