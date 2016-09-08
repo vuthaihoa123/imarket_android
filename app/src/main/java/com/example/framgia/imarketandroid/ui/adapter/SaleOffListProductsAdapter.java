0d0431df4be079a736b775d93881d325375623fd
nguyenvantoan <toannguyen201194@gmail.com>
2016-09-08 17:07:05 +0700

    event_click_floatingbutton

--- app/src/main/java/com/example/framgia/imarketandroid/ui/adapter/SaleOffListProductsAdapter.java
+++ app/src/main/java/com/example/framgia/imarketandroid/ui/adapter/SaleOffListProductsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.framgia.imarketandroid.ui.adapter;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Paint;
